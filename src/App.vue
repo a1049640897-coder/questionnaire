b0bdf7460a45dd299bfe837c9b6840fcb454c4db
13063093621@163.com <a1049640897>
2021-06-27 23:59:13 +0800

    移动端页面完成 1.完成刷新页面跳转

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,16 @@
     <router-view v-wechat-title="$route.meta.title"></router-view>
   </div>
 </template>
+<script>
+export default {
+  name: 'app',
+  components: {
+  },
+  created: function () {
+    this.$router.push('/'); // 注意！ 你这个代码的意思就是每次刷新的时候进入'/login'，去掉就可以了
+  }
+};
+</script>
 
 <style>
   [v-cloak] {
